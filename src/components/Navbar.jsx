7f6f4c41cab755c303d45fbd25f2cb645a81ce2f
amarNrddn <amarnrddn02@gmail.com>
2024-01-28 23:35:47 +0700

    feat: components navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { navbar } from "@/constans/routeNavbar"
+import Link from 'next/link'
+
+const Navbar = () => {
+
+    return (
+        <div className='flex justify-between items-center'>
+            {navbar.map((logo, i) => (
+                <Link
+                    key={i}
+                    href={logo.path}
+                    className='text-primary text-2xl font-semibold'
+                >
+                    {logo.logo}
+                </Link>
+            ))}
+            {navbar.map((nav, i) => {
+                return (
+                    <nav
+                        key={i}
+                        className="flex flex-row text-gray-500 font-semibold items-center"
+                    >
+                        <Link  href={nav.path}>{nav.name}</Link>
+                    </nav>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/constans/routeNavbar.js
@@ -0,0 +1,28 @@
+export const navbar = [
+    {
+        logo: "ST PIZZA",
+        path: "/"
+    },
+    {
+        name: "Home",
+        path: "/home"
+    },
+    {
+        name: "Menu",
+        path: "/menu"
+    },
+    {
+        name: "About",
+        path: "/about"
+    },
+    {
+        name: "Contact",
+        path: "/contact"
+    },
+    {
+        name: "Login",
+        path: "/login",
+        className: "bg-primary px-8 py-2 rounded-full text-white"
+    },
+
+]
